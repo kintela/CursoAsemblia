4af10c0382158812d00e074bd0e512b5a3fe3280
kintela <roberto.quintela@gmail.com>
2018-03-05 18:57:32 +0100

    preba fallida

---
+++ ConsoleCSV/OrderDocument.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CsvHelper;
+
+namespace ConsoleCSV
+{
+    public class OrderDocument
+    {
+        private int id;
+        private int cantidad;
+        private string codigoArticulo;
+
+        public int Id { set => id = value; }
+        public int Cantidad { set => cantidad = value; }
+        public string CodigoArticulo { set => codigoArticulo = value; }
+
+        //public OrderDocument(Stream Document)
+        //{
+
+        //}
+    }
+}
--- ConsoleCSV/Program.cs
+++ ConsoleCSV/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
 
 namespace ConsoleCSV
 {
@@ -6,7 +9,32 @@ namespace ConsoleCSV
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "C:\\borrame\\ejemplo.csv";
+            string fileName = @"C:\borrame\ejemplo.csv";
+
+            List<OrderDocument> orderDocumentList = new List<OrderDocument>();
+
+            using (TextReader filereader = File.OpenText(fileName))
+            {
+                var csv = new CsvReader(filereader);
+                csv.Configuration.HasHeaderRecord = false;
+                while (csv.Read())
+                {
+                    var id = csv.GetField<int>(0);
+                    var cantidad = csv.GetField<int>(1);
+                    var codigoArticulo = csv.GetField<string>(2);
+
+                    OrderDocument od = new OrderDocument();
+
+                    od.Id = id;
+                    od.Cantidad = cantidad;
+                    od.CodigoArticulo = codigoArticulo;
+
+                    orderDocumentList.Add(od);
+                }
+            }
+            
+
         }
     }
 }
