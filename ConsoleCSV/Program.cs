e99c8665d495e86ee24552c4edbe073fdc62aec7
kintela <roberto.quintela@gmail.com>
2018-03-06 09:56:17 +0100

    Correguido

--- ConsoleCSV/Program.cs
+++ ConsoleCSV/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using CsvHelper;
 
 namespace ConsoleCSV
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             string path = "C:\\borrame\\ejemplo.csv";
             string fileName = @"C:\borrame\ejemplo.csv";
@@ -18,6 +19,7 @@ namespace ConsoleCSV
             {
                 var csv = new CsvReader(filereader);
                 csv.Configuration.HasHeaderRecord = false;
+                csv.Configuration.Delimiter = ";";
                 while (csv.Read())
                 {
                     var id = csv.GetField<int>(0);
@@ -32,9 +34,23 @@ namespace ConsoleCSV
 
                     orderDocumentList.Add(od);
                 }
-            }
-            
+            }           
+        }
 
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\borrame\ejemplo.csv";
+
+            List<OrderDocument> orderDocumentList = new List<OrderDocument>();
+
+            using (var sr=new StreamReader(fileName))
+            {
+                var csv = new CsvReader(sr);
+                csv.Configuration.HasHeaderRecord = false;
+                csv.Configuration.Delimiter = ";";
+                IEnumerable<OrderDocument> records = csv.GetRecords<OrderDocument>().ToList();
+               
+            }
         }
     }
 }
