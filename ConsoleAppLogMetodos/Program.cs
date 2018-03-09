cf588157bac020946e1a8c2cc7091550a3ab54e5
kintela <roberto.quintela@gmail.com>
2018-03-09 13:58:40 +0100

    Pruaba ejercici1

--- ConsoleAppLogMetodos/Program.cs
+++ ConsoleAppLogMetodos/Program.cs
@@ -3,10 +3,29 @@
 namespace ConsoleAppLogMetodos
 {
     class Program
-    {
+    {       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool test = RunMethod(Method1);
+
+        }
+
+        public static int Method1(string input)
+        {
+            return 1;
+        }
+
+        public static int Method2(string input)
+        {
+            return 2;
+        }
+
+
+        public static bool RunMethod(Func<string, int> methodName)
+        {
+            int i = methodName("pepe");
+
+            return true;
         }
     }
 }
