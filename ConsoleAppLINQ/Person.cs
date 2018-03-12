b675a5c37d553405af7a41e008771c98085382fe
kintela <roberto.quintela@gmail.com>
2018-03-12 13:14:42 +0100

    Métodos de extension

---
+++ ConsoleAppLINQ/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections.Generic
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+    public static class PersonExtension
+    {
+        public static string FullName(this Person person) => $"{person.Name} {person.LastName}";
+
+        public static int OurCount<T>(this IEnumerable<T> collection) => collection.Count<T>();
+    }
+
+}
---
+++ ConsoleAppLINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.FullName();
+
+            var coleccion = new List<int> { 1, 2, 3 };
+
+            coleccion.OurCount();
+
+            
+        }
+    }
+}
