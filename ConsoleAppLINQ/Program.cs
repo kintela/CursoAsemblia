11c5fbc4ba7c7c37bbf71a33c924dabc2edf11ee
kintela <roberto.quintela@gmail.com>
2018-03-12 14:27:01 +0100

    LINQ

--- ConsoleAppLINQ/Program.cs
+++ ConsoleAppLINQ/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleAppLINQ
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             var person = new Person();
 
@@ -19,6 +19,46 @@ namespace ConsoleAppLINQ
             coleccion.OurCount();
 
             
+        }
+
+        static void Main(string[] args)
+        {
+            var alumnos = new List<Alumno>()
+            {
+                new Alumno(){ Age=21, Name="Iñaki", LastName="Elcoro"},
+                new Alumno(){ Age=23, Name="Robeto", LastName="Quintela"},
+                new Alumno(){ Age=18, Name="Ainara", LastName="Riesco"}
+            };
+
+            var alumnos2 = new List<Alumno>()
+            {
+                new Alumno(){ Age=21, Name="Iñaki", LastName="Elcoro"},
+                new Alumno(){ Age=23, Name="Robeto", LastName="Quintela"},
+                new Alumno(){ Age=18, Name="Ainara", LastName="Riesco"}
+            };
+
+
+            var filtrados = alumnos.Where(a => a.Age < 23).ToList();
+
+            var concatenados = alumnos.Concat(alumnos2).ToList();
+
+            Console.WriteLine(String.Join(",", concatenados.Select(a => a.Name)));
+
+            var alumnosViewmodel= alumnos.Select(a => new AlumnoViewModel()
+            {
+                Age = a.Age,
+                FullName = a.Name + ", " + a.LastName
+            });
+
+
+            var ordenados=alumnosViewmodel.Where(a=>a.Age<23).OrderBy(a => a.Age).ToList();
+
+            var sumaEdades = ordenados.Sum(a => a.Age);
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
